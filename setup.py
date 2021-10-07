55e487424daeb18577c5a45e471e50bcc411680d
dimitrivinet <dimitri.vinet@edu.devinci.fr>
2021-10-07 11:54:46 +0200

    renamed package

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="xarm-hand-control",
+    name="robot-hand-control",
     version="0.0.1",
     author="Dimitri VINET",
     author_email="dimitri.vinet@outlook.com",
-    description="xArm6 control with hand movements",
+    description="Robot control with hand movements",
     long_description=long_description,
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/dimitrivinet/xarm_hand_control/issues",
-    },
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/dimitrivinet/xarm_hand_control/issues",
+    # },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
